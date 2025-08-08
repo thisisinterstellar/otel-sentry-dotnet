564f3597745f6c133c32d444580bfdc1df904e74
Tim Fisicaro <40387575+tfisicaro@users.noreply.github.com>
2025-08-08 21:33:54 +0200

    refactor: add pragma disable on file

--- Isg.Api/Program.cs
+++ Isg.Api/Program.cs
@@ -1,13 +1,11 @@
+#pragma warning disable SENTRY0001
 var builder = WebApplication.CreateBuilder(args);
 
 SentrySdk.Init(options =>
 {
     options.Dsn = "https://examplePublicKey@o0.ingest.sentry.io/0";
-#pragma warning disable SENTRY0001
     options.Experimental.EnableLogs = true;
-#pragma warning restore SENTRY0001
 });
-
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
