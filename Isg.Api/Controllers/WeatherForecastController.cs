25c59d697066a8703521c1f42ed5385babdb7a19
Tim Fisicaro <40387575+tfisicaro@users.noreply.github.com>
2025-08-08 21:16:50 +0200

    feat: add multiple demo projects

---
+++ Isg.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Isg.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        _logger.LogDebug("Debugging weather forecast data retrieval");
+        _logger.LogInformation("Fetching weather forecast data");
+        _logger.LogWarning("This is a warning message for weather forecast retrieval");
+        _logger.LogError("An error occurred while retrieving weather forecast data");
+        _logger.LogCritical("Critical issue in weather forecast retrieval");
+        
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
